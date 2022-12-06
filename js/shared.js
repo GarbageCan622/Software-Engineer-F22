443ee5dcc3b4c358c6a75f30669e18ec6e371594
GarbageCan622 <89987964+GarbageCan622@users.noreply.github.com>
2022-12-06 18:09:42 -0500

    Add files via upload

--- js/shared.js
+++ js/shared.js
@@ -12,56 +12,94 @@ if (localStorage.getItem('MQuiz1') == null) {
     localStorage.setItem('MQuiz1', '-');
 }
 
-//English Course
-var ELS = [90, 80, 70, 60];	//English letter scale
+/*English Course*/ {
+	var ELS = [90, 80, 70, 60];			//English letter scale
+	if (localStorage.getItem('ELS') == null) {
+		localStorage.setItem('ELS', JSON.stringify(ELS));	//In order to store more than just strings, JSON.stringify must be called on the object or data structure
+	}														//Map objects don't seem to work with stringify
 
-const EHW1 = new Map();	//English Homework 1
-EHW1.set(1,88.8);	//First value is student ID, second value is grade
-EHW1.set(2,83.2);
-EHW1.set(3,79.9);
-
-const EHW2 = new Map();	//English Homework 2
-EHW1.set(1,100);
-EHW1.set(2,95);
-EHW1.set(3,88);
-
-const EP1 = new Map();	//English Paper 1
-EHW1.set(1,90);
-EHW1.set(2,85);
-EHW1.set(3,82.5);
-
-const EP2 = new Map();	//English Paper 2
-EHW1.set(1,88);
-EHW1.set(2,86.3);
-EHW1.set(3,92.5);
-
-const EFP = new Map();	//English Final Paper
-EHW1.set(1,85.5);
-EHW1.set(2,83);
-EHW1.set(3,84.2);
-
-if(localStorage.getItem('ELS' == null) {
-   localStorage.setItem('ELS', JSON.stringify(ELS));
+	var ECats_names = ['homework', 'paper', 'final paper'];	//Category names (Correspond to eachother, (homework, 20), (paper, 40)...
+	var ECats_num = [20, 40, 40];							//Category weights
+	if (localStorage.getItem('ECats_names') == null) {
+		localStorage.setItem('ECats_names', JSON.stringify(ECats_names));
+	}
+	if (localStorage.getItem('ECats_num') == null) {
+		localStorage.setItem('ECats_num', JSON.stringify(ECats_num));
+	}
+	
+	var ESIDs = [1,2,3];		//English course student ID's
+	if (localStorage.getItem('ESIDs') == null) {
+		localStorage.setItem('ESIDs', JSON.stringify(ESIDs));
+	}
+	
+	var EHW1 = [88, 90, 75, 88.88888, 100, 100, 83, 87, 70, 60];	//HW1 grades (corresponds to SIDs)
+	if (localStorage.getItem('EHW1') == null) {
+		localStorage.setItem('EHW1', JSON.stringify(EHW1));
+	}
+	
+	var EHW2 = [85, 82.5, 100, 50, 73, 84, 63, 95, 92, 75];		//HW2
+	if (localStorage.getItem('EHW2') == null) {
+		localStorage.setItem('EHW2', JSON.stringify(EHW2));
+	}
+	
+	var EP1 = [78, 81.5, 83.5, 95, 90, 88, 77.5, 90, 91, 89];		//Paper 1 grades
+	if (localStorage.getItem('EP1') == null) {
+		localStorage.setItem('EP1', JSON.stringify(EP1));
+	}
+	
+	var EPF = [88, 87.5, 91, 95, 95, 85, 72, 74, 83, 89];		//Final paper grades
+	if (localStorage.getItem('EPF') == null) {
+		localStorage.setItem('EPF', JSON.stringify(EPF));
+	}
 }
 
-if (localStorage.getItem('EHW1' == null) {
-	localStorage.setItem('EHW1', JSON.stringify(EHW1));	//In order to store more than just strings, JSON.stringify must be called on the object or data structure
-}
+/*Math Course*/ {
+	var MLS = [90, 80, 70, 60];			//Letter scale
+	if (localStorage.getItem('MLS') == null) {
+		localStorage.setItem('MLS', JSON.stringify(MLS));	//In order to store more than just strings, JSON.stringify must be called on the object or data structure
+	}														//Map objects don't seem to work with stringify
 
-if (localStorage.getItem('EHW2' == null) {
-	localStorage.setItem('EHW2', JSON.stringify(EHW2));
+	var MCats_names = ['homework', 'paper', 'final paper'];	//Category names (Correspond to eachother, (homework, 20), (paper, 40)...
+	var MCats_num = [15, 20, 30, 40];							//Category weights
+	if (localStorage.getItem('MCats_names') == null) {
+		localStorage.setItem('MCats_names', JSON.stringify(MCats_names));
+	}
+	if (localStorage.getItem('MCats_num') == null) {
+		localStorage.setItem('MCats_num', JSON.stringify(MCats_num));
+	}
+	
+	var MSIDs = [1,2,3, 4, 5, 6, 7, 8, 9, 10];		//course student ID's
+	if (localStorage.getItem('MSIDs') == null) {
+		localStorage.setItem('MSIDs', JSON.stringify(MSIDs));
+	}
+	
+	var CW1 = [100,100,100,100,100,90,100,80,90,90];		//Classwork 1
+	if (localStorage.getItem('CW1') == null) {
+		localStorage.setItem('CW1', JSON.stringify(CW1));
+	}
+	
+	var CW2 = ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'];	//Classwork 2
+	if (localStorage.getItem('CW2') == null) {
+		localStorage.setItem('CW2', JSON.stringify(CW2));
+	}
+	
+	var MHW1 = [100, 95, 92, 85, 90, 92, 82, 75, 0, 60];		//Homework 1
+	if (localStorage.getItem('MHW1') == null) {
+		localStorage.setItem('MHW1', JSON.stringify(MHW1));
+	}
+	
+	var MHW2 = [85, 88, 76, 90, 85, 85, 82, 73, 90, 79];		//Homework 2
+	if (localStorage.getItem('MHW2') == null) {
+		localStorage.setItem('MHW2', JSON.stringify(MHW2));
+	}
+	
+	var MQ1 = [100,100,95,100,90,88,85,80,82,88];				//Quiz 1
+	if (localStorage.getItem('MQ1') == null) {
+		localStorage.setItem('MQ1', JSON.stringify(MQ1));
+	}
+	
+	var ME1 = [75,95,90,100,92,84,87,88,80,79];				//Exam 1
+	if (localStorage.getItem('ME1') == null) {
+		localStorage.setItem('ME1', JSON.stringify(ME1));
+	}	
 }
-
-if (localStorage.getItem('EP1' == null) {
-	localStorage.setItem('EP1', JSON.stringify(EP1));
-}
-
-if (localStorage.getItem('EP2' == null) {
-	localStorage.setItem('EP2', JSON.stringify(EP2));
-}
-
-if (localStorage.getItem('EFP' == null) {
-	localStorage.setItem('EFP', JSON.stringify(EFP));
-}
-
-//Math Course
