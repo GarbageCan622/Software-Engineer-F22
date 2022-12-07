078ce3322087a1f4194b73e31a3d0e84d93d2cc8
GarbageCan622 <89987964+GarbageCan622@users.noreply.github.com>
2022-12-07 10:06:05 -0500

    Update shared.js

--- js/shared.js
+++ js/shared.js
@@ -1,17 +1,4 @@
-
-if (localStorage.getItem('EHomework1') == null) { //this null check allows us to set the value of the localStorage originally on page load, after page load it wont be null anymore so when you go to index.html again none of the ifs will fire. important for keeping the value the same when going in between tabs
-    localStorage.setItem('EHomework1', '20');
-}
-if (localStorage.getItem('EQuiz1') == null) {
-    localStorage.setItem('EQuiz1', '-');
-}
-if (localStorage.getItem('MHomework1') == null) {
-    localStorage.setItem('MHomework1', '20');
-}
-if (localStorage.getItem('MQuiz1') == null) {
-    localStorage.setItem('MQuiz1', '-');
-}
-
+localStorage.clear();
 /*English Course*/ {
 	var ELS = [90, 80, 70, 60];			//English letter scale
 	if (localStorage.getItem('ELS') == null) {
